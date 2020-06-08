882b6a82de739be52341fd9ae513dbde5cf04337
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-08 23:03:15 +0200

    Ajout de l'action permettant de récupérer via API rest les taux de change entre une monnaie d'un pays et le dollar entre il y a un mois et aujourd'hui. On retourne le tableau de taux de change et les infos du pays

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
+
 export const GET_COUNTRIES = 'GET_COUNTRIES'
+export const GET_RATE_EXCHANGE = "GET_RATE_EXCHANGE";
+
 export function fetchCountries(){
   return function (dispatch) {
     axios.get('https://restcountries.eu/rest/v2/all')
@@ -7,4 +10,26 @@ export function fetchCountries(){
         dispatch({ type: GET_COUNTRIES, payload: axiosResponse.data })
       })
   }
+}
+
+export function fetchRateExchange(country) {
+  return function(dispatch) {
+    axios.get(`https://api.exchangeratesapi.io/history?start_at=${getLastMonth()}&end_at=${formatedDate(new Date())}&base=USD&symbols=${country.currencyCode}`)
+      .then(axiosResponse => {
+        dispatch({
+          type: GET_RATE_EXCHANGE,
+          payload: { rates: axiosResponse.data.rates, ...country }
+        })
+      })
+  }
+}
+
+function formatedDate(date) {
+  return date.toISOString().split("T")[0]
+}
+
+function getLastMonth() {
+  let now = new Date()
+  now.setMonth(now.getMonth() - 1)
+  return formatedDate(now)
 }
