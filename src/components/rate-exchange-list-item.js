d7d9a46dd94bc5aea32f99a80b3dab11749c4a73
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-09 01:10:04 +0200

    Affichage du drapeau dans l'item de liste de taux de change. Installation de react-chartkick et chart.js pour préparer la création du graphique

--- src/components/rate-exchange-list-item.js
+++ src/components/rate-exchange-list-item.js
@@ -2,12 +2,11 @@ import React, { Component } from "react"
 
 class RateEchangeListItem extends Component {
   render() {
-    console.log(this.props.rateExchange)
+    const { flag, name } = this.props.rateExchange
     return (
       <tr>
-        <td>
-          {this.props.rateExchange.name}
-        </td>
+        <td>{name}<br /><img src={flag} height="60" width="100" /></td>
+        <td>Graphique</td>
       </tr>
     )
   }
