502fa72d44e7ce4c1fe9b4184f7d3162187eb56f
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-08 22:08:07 +0200

    Modif du countries reducer pour ne stocker dans le state que 4 infos du get API et ne retenir que les pays dont la monnaie est différente de USD

--- src/reducers/reducer-countries.js
+++ src/reducers/reducer-countries.js
@@ -6,7 +6,20 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch(action.type) {
-    case GET_COUNTRIES : return { ...state, countries: action.payload }
+    case GET_COUNTRIES : return { ...state, countries: getCountriesInfo(action.payload) }
     default : return state
   }
+}
+
+function getCountriesInfo (data) {
+  return data.map(c => {
+    return {
+      name: c.name,
+      currencyCode: c.currencies[0].code,
+      flag: c.flag,
+      code: c.alpha3Code
+    }
+  }).filter(c => {
+    return c.currencyCode !== 'USD'
+  })
 }
