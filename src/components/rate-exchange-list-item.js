a15e4e592ae8c0877d6455a2729cd7124b99c697
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-09 01:23:38 +0200

    Affichage du graphique via react-chartkick et chart.js reprenant tous les taux de change récupérés sur le dernier mois entre la monnaie du pays et le dollar US

--- src/components/rate-exchange-list-item.js
+++ src/components/rate-exchange-list-item.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react"
+import ReactChartKick, { AreaChart } from "react-chartkick"
+import Chart from "chart.js"
+
+ReactChartKick.addAdapter(Chart)
 
 class RateEchangeListItem extends Component {
   render() {
-    const { flag, name } = this.props.rateExchange
+    const { flag, name, rates, currencyCode } = this.props.rateExchange
     return (
       <tr>
-        <td>{name}<br /><img src={flag} height="60" width="100" /></td>
-        <td>Graphique</td>
+        <td>{name}<br /><img src={flag} height="60" width="100" alt='non trouvée' /></td>
+        <td className="col-md-12">
+          <AreaChart
+            data={formatData(rates, currencyCode)}
+            xtitle="Date"
+            ytitle={currencyCode}
+          />
+        </td>
       </tr>
     )
   }
+}
+
+function formatData(rates, currencyCode) {
+  return Object.keys(rates).map(date => {
+    return [date, rates[date][currencyCode]]
+  })
 }
 
 export default RateEchangeListItem
